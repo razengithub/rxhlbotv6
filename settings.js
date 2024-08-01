aff085490b09e581c7e2e08983b5fa3e1d203715
razengithub <dirastore749@gmail.com>
2024-08-01 12:19:15 +0700

    upload via termux

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6288212220573']
+global.botname = 'Zenx Bot'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Zenx Official"
+global.sticker2 = "🌜"
